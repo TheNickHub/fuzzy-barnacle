a496d654c4c236f888484a78a5796221f4ed2731
TheNickHub <shenn18@terpmail.umd.edu>
2023-05-11 19:36:32 -0400

    fuzzy

--- client/script.js
+++ client/script.js
@@ -8,14 +8,14 @@ function injectHTML(list) {
   console.log("fired injectHTML");
   const target = document.querySelector("#hospital_list");
   target.innerHTML = "";
-  list.forEach((item) => {
+  list.forEach(function (item) {
     const str = `<li>${item.name}</li>`;
     target.innerHTML += str;
   });
 }
 
 function filterList(list, query) {
-  return list.filter((item) => {
+  return list.filter(function (item) {
     const lowerCaseName = item.name.toLowerCase();
     const lowerCaseQuery = query.toLowerCase();
     return lowerCaseName.includes(lowerCaseQuery);
@@ -25,22 +25,93 @@ function filterList(list, query) {
 function cutHospitalList(list) {
   console.log("fired cut list");
   const range = [...Array(15).keys()];
-  return (newArray = range.map((item) => {
+  return (newArray = range.map(function (item) {
     const index = getRandomIntInclusive(0, list.length - 1);
     return list[index];
   }));
 }
 
-function initMap() {
-  // 38.9072° N, 77.0369° W
-  const carto = L.map("map").setView([38.98, -76.93], 13);
+let mapInitialized = false;
+let carto;
+
+function initMap(city) {
+  if (carto) {
+    carto.remove();
+  }
+  carto = L.map("map").setView([39.0458, -76.6413], 7);
   L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 19,
     attribution:
-      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>",
+    maxZoom: 18,
   }).addTo(carto);
-  return carto;
+
+  fetch(
+    "https://www.communitybenefitinsight.org/api/get_hospitals.php?state=MD"
+  )
+    .then((response) => response.json())
+    .then((hospitals) => {
+      const hospitalIcon = L.icon({
+        iconUrl:
+          "https://cdn3.iconfinder.com/data/icons/medical-icons-3/512/Hospital-512.png",
+        iconSize: [40, 40],
+      });
+
+      const filteredHospitals = hospitals.filter(
+        (hospital) => hospital.city === city
+      );
+
+      for (const hospital of filteredHospitals) {
+        const address =
+          hospital.street_address +
+          ", " +
+          hospital.city +
+          ", " +
+          hospital.state +
+          " " +
+          hospital.zip_code;
+        const url = `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(
+          address
+        )}&key=fdbb867d57b24fb5b77b706db67845a1`;
+        fetch(url)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            const lat = data.results[0].geometry.lat;
+            const lng = data.results[0].geometry.lng;
+            const marker = L.marker([lat, lng]).bindPopup(hospital.name);
+            marker.addTo(carto);
+          })
+          .catch((error) => console.error(error));
+      }
+
+      // Zoom the map
+      const url = `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(
+        city + ", Maryland"
+      )}&key=fdbb867d57b24fb5b77b706db67845a1`;
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          const lat = data.results[0].geometry.lat;
+          const lng = data.results[0].geometry.lng;
+          carto.setView([lat, lng], 11);
+        })
+        .catch((error) => console.error(error));
+    });
 }
+
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  const city = document.querySelector("city");
+  initMap(city);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const selectCity = document.querySelector("#cities");
+  selectCity.addEventListener("change", (event) => {
+    const selectedCity = event.target.value;
+    initMap(selectedCity);
+  });
+});
 
 function markerPlace(array, map) {
   console.log("array for markers", array);
@@ -61,12 +132,12 @@ function markerPlace(array, map) {
 
 async function mainEvent() {
   // the async keyword means we can make API requests
-  const mainForm = document.querySelector(".main_form");
-  const loadDataButton = document.querySelector("#data_load");
-  const clearDataButton = document.querySelector("#data_clear");
+  // const mainForm = document.querySelector(".main_form");
+  // const loadDataButton = document.querySelector("#data_load");
+  // const clearDataButton = document.querySelector("#data_clear");
   const generateListButton = document.querySelector("#generate");
-  const textField = document.querySelector("#hosp");
-  const filterButton = document.querySelector('#filter')
+  // const textField = document.querySelector("filter");
+  const citiesDropdown = document.querySelector("#cities");
 
   //const loadAnimation = document.querySelector("#data_load_animation");
   //loadAnimation.style.display = "none";
@@ -74,73 +145,83 @@ async function mainEvent() {
 
   const carto = initMap();
 
-  const storedData = localStorage.getItem("storedData");
-  let parsedData = JSON.parse(storedData);
-  if (parsedData?.length > 0) {
-    generateListButton.classList.remove("hidden");
+  // const storedData = localStorage.getItem("storedData");
+  // let parsedData = JSON.parse(storedData);
+  // if (parsedData?.length > 0) {
+  //   generateListButton.classList.remove("hidden");
+  // }
+  localStorage.clear();
+  let storedList = localStorage.getItem("hospitalList");
+  if (!storedList) {
+    const results = await fetch(
+      "https://www.communitybenefitinsight.org/api/get_hospitals.php?state=MD"
+    );
+    storedList = await results.json();
+    localStorage.setItem("hospitalList", storedList);
   }
 
   let currentList = [];
-
-  /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
-  loadDataButton.addEventListener("click", async (submitEvent) => {
-    // async has to be declared on every function that needs to "await" something
-    console.log("Loading Data");
-    loadAnimation.style.display = "inline-block";
-
-    // Basic GET request - this replaces the form Action
-    const results = await fetch(
-      "https://data.princegeorgescountymd.gov/resource/county-hospitals.json"
-    );
-
-    filterDataButton.addEventListener('click', (event) => {
-      console.log('clicked FilterButton');
-      
-      const formData = new FormData(mainForm);
-      const formProps = Object.fromEntries(formData);
-      
-      console.log(formProps);
   
-      const newList = filterList(currentList, formProps.filter);
-      console.log(newList);
-      injectHTML(newList);
-  
-    })
 
-    // This changes the response from the GET into data we can use - an "object"
-    const storedList = await results.json();
-    localStorage.setItem("storedData", JSON.stringify(storedList));
-    parsedData = storedList;
+  function filterCity(city) {
+    const matched = storedList.filter((hospital) => {
+      return hospital.city.toLowerCase() === city.toLowerCase();
+    });
+    return matched;
+  }
 
-    if (parsedData?.length > 0) {
-      generateListButton.classList.remove("hidden");
-    }
-
-    loadAnimation.style.display = "none";
-    // console.table(storedList);
+  console.log(storedList);
+  let data = new Set();
+  storedList.forEach((hospital) => {
+    data.add(hospital.city);
   });
 
-  /*generateListButton.addEventListener("click", (event) => {
-    console.log("generate new list");
-    currentList = cutHospitalList(parsedData);
-    console.log(currentList);
-    injectHTML(currentList);
-    markerPlace(currentList, carto);
+  console.log(data);
+
+  data.forEach((city) => {
+    const option = document.createElement("option");
+    option.text = city;
+    option.value = city;
+    citiesDropdown.add(option);
   });
 
-  textField.addEventListener("input", (event) => {
-    console.log("input", event.target.value);
-    const newList = filterList(currentList, event.target.value);
-    console.log(newList);
-    injectHTML(newList);
-    markerPlace(newList, carto);
+  let selectedCity = "";
+
+  const dropdownMenu = document.querySelector("#cities");
+  console.log(dropdownMenu);
+  dropdownMenu.addEventListener("change", (event) => {
+    selectedCity = event.target.value;
   });
 
-  clearDataButton.addEventListener("click", (event) => {
-    console.log("clear browser data");
+  generateListButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const filteredList = filterCity(selectedCity);
+    console.log(filteredList);
+    injectHTML(filteredList);
+  });
+
+  const clearDataButton = document.getElementById("clear");
+  const hospitalList = document.getElementById("hospitalList");
+  const hospitalMap = document.getElementById("hospitalMap");
+
+  clearDataButton.addEventListener("click", async (event) => {
     localStorage.clear();
-    console.log("localStorage Check", localStorage.getItem("storedData"));
-  });*/
+
+    let storedData = localStorage.getItem("hospitalList");
+    if (!storedData) {
+      const results = await fetch(
+        "https://www.communitybenefitinsight.org/api/get_hospitals.php?state=MD"
+      );
+      const storedList = await results.json();
+      localStorage.setItem("hospitalList", JSON.stringify(storedList));
+
+      const hospitalList = document.getElementById("hospital_list");
+      hospitalList.innerHTML = "";
+
+      const hospitalMap = document.getElementById("map");
+      hospitalList.innerHTML = "";
+    }
+  });
 }
 
 document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests
